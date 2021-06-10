e535d7774165400a72a7675e328d86d420d86703
Adrian Cieplucha <adik.cieplucha@gmail.com>
2021-06-10 11:51:24 +0200

    updated

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -1,82 +1,54 @@
-const input = document.querySelectorAll(".input");
-// const input1 = document.querySelector(".input1");
-// const input2 = document.querySelector(".input2");
-// const input3 = document.querySelector(".input3");
 const main = document.querySelector("main");
+const input1 = document.querySelector("#input1");
+const input2 = document.querySelector("#input2");
+const input3 = document.querySelector("#input3");
 
-input.forEach((input) => {
-  input.addEventListener("click", () => {
-    input.classList.add("active");
+function makeInputActive() {
+  input1.addEventListener("click", () => {
+    input1.classList.add("active");
+    input2.classList.remove("active");
+    input3.classList.remove("active");
   });
-});
 
-function checkColorTheme() {
-  if (input[0].classList.contains("active")) {
+  input2.addEventListener("click", () => {
+    input2.classList.add("active");
+    input1.classList.remove("active");
+    input3.classList.remove("active");
+  });
+
+  input3.addEventListener("click", () => {
+    input3.classList.add("active");
+    input1.classList.remove("active");
+    input2.classList.remove("active");
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    input1.classList.add("active");
+  });
+}
+
+function setColorTheme() {
+  if (input1.classList.contains("active")) {
     main.classList.add("dark");
   } else {
     main.classList.remove("dark");
   }
-
-  if (input[1].classList.contains("active")) {
+  if (input2.classList.contains("active")) {
     main.classList.add("light");
   } else {
     main.classList.remove("light");
   }
-
-  if (input[2].classList.contains("saturated")) {
+  if (input3.classList.contains("saturated")) {
     main.classList.add("saturated");
   } else {
     main.classList.remove("saturated");
   }
-
-  console.log(input[0]);
-  console.log(input[1]);
-  console.log(input[2]);
+  console.log(input1);
+  console.log(input2);
+  console.log(input3);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  input[0].classList.add("active");
-});
+console.log(main);
 
-// input1.addEventListener("click", () => {
-//   input1.classList.add("active");
-//   //   input2.classList.remove("active");
-//   //   input3.classList.remove("active");
-// });
-// input2.addEventListener("click", () => {
-//   input2.classList.add("active");
-//   //   input1.classList.remove("active");
-//   //   input3.classList.remove("active");
-// });
-// input3.addEventListener("click", (e) => {
-//   input3.classList.add("active");
-//   console.log(e);
-//   //   input1.classList.remove("active");
-//   //   input2.classList.remove("active");
-// });
-
-// function checkColorTheme() {
-//   if (input1.classList.contains("active")) {
-//     main.classList.add("dark");
-//   } else {
-//     main.classList.remove("dark");
-//   }
-
-//   if (input2.classList.contains("active")) {
-//     main.classList.add("light");
-//   } else {
-//     main.classList.remove("light");
-//   }
-
-//   if (input3.classList.contains("active")) {
-//     main.classList.add("saturated");
-//   } else {
-//     main.classList.remove("saturated");
-//   }
-
-//   console.log(input1);
-//   console.log(input2);
-//   console.log(input3);
-// }
-
-checkColorTheme();
+setColorTheme();
+makeInputActive();
