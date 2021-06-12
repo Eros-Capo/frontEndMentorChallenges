e2b56f284a0e7c7826bc0b513e04562e98ffcde4
Adrian Cieplucha <adik.cieplucha@gmail.com>
2021-06-12 18:38:12 +0200

    still having a problem

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -1,38 +1,27 @@
-let main = document.querySelector("main");
-const input1 = document.querySelector("#input1");
-const input2 = document.querySelector("#input2");
-const input3 = document.querySelector("#input3");
-
-// window.onload = function () {
-//   setColorTheme();
-// };
-
-function setColorTheme() {
-  // document.addEventListener("DOMContentLoaded", () => {
-  //   input1.checked = true;
-  // });
-
-  input1.checked = true;
+document.addEventListener("DOMContentLoaded", () => {
+  let main = document.getElementById("main");
+  let input1 = document.getElementById("input1");
+  let input2 = document.getElementById("input2");
+  let input3 = document.getElementById("input3");
 
   if (input1.checked == true) {
     main.classList.add("dark");
-    console.log("hello");
   } else {
-    input1.checked = false;
     main.classList.remove("dark");
   }
   if (input2.checked == true) {
     main.classList.add("light");
+    console.log("hello2");
   } else {
-    input2.checked = false;
     main.classList.remove("light");
   }
   if (input3.checked == true) {
     main.classList.add("saturated");
+    console.log("hello3");
   } else {
-    input3.checked = false;
     main.classList.remove("saturated");
   }
-}
 
-setColorTheme();
+  console.log(main);
+  console.log(input1);
+});
